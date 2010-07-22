4212d40eb0b76f3164fd76b92eddea619e14fef6
Brandon Mitchell <brandon@twitter.com>
2010-07-22 14:19:58 -0700

    Convert Reliable proxy to new get style.

--- lib/kestrel/client/reliable.rb
+++ lib/kestrel/client/reliable.rb
@@ -1,8 +1,11 @@
 module Kestrel
   class Client
     class Reliable < Proxy
-      def get(key, *args, &block)
-        client.get(key + "/close/open", *args)
+
+      def get(key, opts = false)
+        opts = extract_options(opts)
+        opts.merge! :close => true, :open => true
+        client.get(key, opts)
       end
 
       def abort(key)
