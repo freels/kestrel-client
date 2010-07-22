ff8215367d5d66c4700ff9a4e94202b4eff8ec3e
Brandon Mitchell <brandon@twitter.com>
2010-07-22 14:19:40 -0700

    Convert Blocking proxy to new get style and remove sleep().

--- lib/kestrel/client/blocking.rb
+++ lib/kestrel/client/blocking.rb
@@ -1,31 +1,22 @@
 module Kestrel
   class Client
     class Blocking < Proxy
-      DEFAULT_EXPIRY = 0
-      WAIT_TIME_BEFORE_RETRY = 0.25
+      DEFAULT_TIMEOUT = 250
 
-      def get(*args)
-        while !(response = client.get(*args))
-          sleep WAIT_TIME_BEFORE_RETRY
+      def get(key, opts = false)
+        opts = extract_options(opts)
+        opts[:timeout] = DEFAULT_TIMEOUT
+
+        loop do
+          response = client.get(key, opts)
+          return response if response
         end
-        response
       end
 
       def get_without_blocking(*args)
         client.get(*args)
       end
 
-      def set(key, value, expiry = DEFAULT_EXPIRY, raw = false)
-        @retried = false
-        begin
-          client.set(key, value, expiry, raw)
-        rescue Memcached::Error => e
-          raise if @retried
-          sleep(WAIT_TIME_BEFORE_RETRY)
-          @retried = true
-          retry
-        end
-      end
     end
   end
 end
