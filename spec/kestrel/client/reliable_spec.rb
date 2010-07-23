bb6cd6efdc17cc015a1aed06fd5c21ba9d157659
Brandon Mitchell <brandon@twitter.com>
2010-07-22 17:11:52 -0700

    Specs for things done.

--- spec/kestrel/client/reliable_spec.rb
+++ spec/kestrel/client/reliable_spec.rb
@@ -5,13 +5,11 @@ describe "Kestrel::Client::Reliable" do
     before do
       @raw_kestrel_client = Kestrel::Client.new(*Kestrel::Config.default)
       @kestrel = Kestrel::Client::Reliable.new(@raw_kestrel_client)
+      stub(@kestrel).rand { 1 }
+      @queue = "some_queue"
     end
 
     describe "#get" do
-      before do
-        @queue = "some_queue"
-        stub(@kestrel).rand { 1 }
-      end
 
       it "asks for a transaction" do
         mock(@raw_kestrel_client).get(@queue, :raw => false, :open => true, :close => true) { :mcguffin }
@@ -65,5 +63,68 @@ describe "Kestrel::Client::Reliable" do
 
     end
 
+    describe "#current_try" do
+
+      it "returns 1 if nothing has been gotten" do
+        @kestrel.current_try.should == 1
+      end
+
+      it "returns 1 for jobs that have not been retried" do
+        mock(@raw_kestrel_client).get(@queue, anything) { :mcguffin }
+        @kestrel.get(@queue)
+        @kestrel.current_try.should == 1
+      end
+
+      it "returns 1 plus the number of tries for a RetryableJob" do
+        stub(@kestrel).rand { 0 }
+        mock(@raw_kestrel_client).get(@queue + "_errors", anything) do
+          Kestrel::Client::Reliable::RetryableJob.new(1, :mcmuffin)
+        end
+        @kestrel.get(@queue)
+        @kestrel.current_try.should == 2
+      end
+
+    end
+
+    describe "#retry" do
+      before do
+        stub(@raw_kestrel_client).get(@queue, anything) { :mcmuffin }
+        @kestrel.get(@queue)
+      end
+
+      it "enqueues a fresh failed job to the errors queue with a retry count" do
+        mock(@raw_kestrel_client).set(@queue + "_errors", anything) do |queue, job|
+          job.retries.should == 1
+          job.job.should == :mcmuffin
+        end
+        @kestrel.retry.should be_true
+      end
+
+      it "increments the retry count and re-enqueues the retried job" do
+        stub(@kestrel).rand { 0 }
+        stub(@raw_kestrel_client).get(@queue + "_errors", anything) do
+          Kestrel::Client::Reliable::RetryableJob.new(1, :mcmuffin)
+        end
+
+        mock(@raw_kestrel_client).set(@queue + "_errors", anything) do |queue, job|
+          job.retries.should == 2
+          job.job.should == :mcmuffin
+        end
+
+        @kestrel.get(@queue)
+        @kestrel.retry.should be_true
+      end
+
+      it "does not enqueue the retried job after too many tries" do
+        stub(@kestrel).rand { 0 }
+        stub(@raw_kestrel_client).get(@queue + "_errors", anything) do
+          Kestrel::Client::Reliable::RetryableJob.new(Kestrel::Client::Reliable::DEFAULT_RETRIES - 1, :mcmuffin)
+        end
+        mock(@raw_kestrel_client).set(@queue + "_errors", anything).never
+        @kestrel.get(@queue)
+        @kestrel.retry.should be_false
+      end
+
+    end
   end
 end
