b70f4505b040096bbb6627c951a6efb3029b7175
Matt Freels <matt@freels.name>
2010-08-26 10:50:15 -0700

    somewhat exponential randomized backoff in the blocking proxy

--- lib/kestrel/client/blocking.rb
+++ lib/kestrel/client/blocking.rb
@@ -1,19 +1,21 @@
 module Kestrel
   class Client
     class Blocking < Proxy
-      SLEEP_TIME = 0.5
+
+      # random backoff sleeping
+
+      SLEEP_TIMES = [[0] * 1, [0.01] * 2, [0.1] * 2, [0.5] * 2, [1.0] * 1].flatten
 
       def get(*args)
-        times = 0
+        count = 0
 
-        loop do
-          times += 1
+        while count += 1
 
           if response = client.get(*args)
             return response
           end
 
-          sleep SLEEP_TIME if times > 5
+          sleep_for_count(count)
         end
       end
 
@@ -21,6 +23,14 @@ module Kestrel
         client.get(*args)
       end
 
+      private
+
+      def sleep_for_count(count)
+        base = SLEEP_TIMES[count] || SLEEP_TIMES.last
+
+        time = ((rand * base) + base) / 2
+        sleep time if time > 0
+      end
     end
   end
 end
