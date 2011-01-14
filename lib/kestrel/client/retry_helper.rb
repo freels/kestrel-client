0a84e9c4e195500f104daf8c9d1cea0bb3f26ae0
Wanli Yang <wanli@TW-MBP13-WYang.local>
2011-01-14 12:07:34 -0800

    get should return nil for all recoverable errors

--- lib/kestrel/client/retry_helper.rb
+++ lib/kestrel/client/retry_helper.rb
@@ -1,26 +1,10 @@
 module Kestrel::Client::RetryHelper
 
-  # Exceptions which are connection failures we retry after
-  RECOVERABLE_ERRORS = [
-    Memcached::ServerIsMarkedDead,
-    Memcached::ATimeoutOccurred,
-    Memcached::ConnectionBindFailure,
-    Memcached::ConnectionFailure,
-    Memcached::ConnectionSocketCreateFailure,
-    Memcached::Failure,
-    Memcached::MemoryAllocationFailure,
-    Memcached::ReadFailure,
-    Memcached::ServerError,
-    Memcached::SystemError,
-    Memcached::UnknownReadFailure,
-    Memcached::WriteFailure
-  ]
-
   private
 
   def with_retries #:nodoc:
     yield
-  rescue *RECOVERABLE_ERRORS
+  rescue *Kestrel::Client::RECOVERABLE_ERRORS
     tries ||= @exception_retry_limit + 1
     tries -= 1
     tries > 0 ? retry : raise
